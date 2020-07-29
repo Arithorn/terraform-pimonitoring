81407fa62fb3366f85693da8110e2674dd6f44a2
Peet Gouws <peet@gouws.com>
2020-07-29 14:04:46 +0200

    changed key_name into variable

--- compute.tf
+++ compute.tf
@@ -13,17 +13,17 @@ data "aws_ami" "al2" {
   owners = ["amazon"]
 }
 resource "aws_instance" "dockerhost" {
-    ami           = data.aws_ami.al2.id
-    instance_type = var.instance_type
-    user_data = file("user_data")
-    security_groups = [aws_security_group.tls_group.name,aws_security_group.grafana_host_group.name]
-    key_name = "EC2 Tutorial"
-    tags = var.tags
-    depends_on = [aws_secretsmanager_secret_version.efs]
-    iam_instance_profile = aws_iam_instance_profile.docker_profile.name
+  ami                  = data.aws_ami.al2.id
+  instance_type        = var.instance_type
+  user_data            = file("user_data")
+  security_groups      = [aws_security_group.tls_group.name, aws_security_group.grafana_host_group.name]
+  key_name             = var.key_name
+  tags                 = var.tags
+  depends_on           = [aws_secretsmanager_secret_version.efs]
+  iam_instance_profile = aws_iam_instance_profile.docker_profile.name
 
 }
 
 output "ami" {
-    value = [data.aws_ami.al2.id]
+  value = [data.aws_ami.al2.id]
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
 variable "aws_region" {
-    type = string
-    default ="af-south-1"
+  type    = string
+  default = "af-south-1"
 }
 
 variable "tags" {
-    default = {
-        environment="pitest"
-    }
+  default = {
+    environment = "pitest"
+  }
 }
 
 variable "instance_type" {
-    type    =string
-    default = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
 
+variable "key_name" {
+  type    = string
+  default = "EC2 Tutorial"
+}
